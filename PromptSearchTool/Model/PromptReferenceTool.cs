6d027ffa67273f5be50a660e38e4b144e92fab2d
a <a>
2024-04-07 12:23:40 +0900

    プロンプト検索ツール（途中）をコミット

---
+++ PromptSearchTool/Model/PromptReferenceTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace PromptSearchTool.Model
+{
+    [Table(Name = "PROMPT_TABLE")]
+    class ReferencePronptTable
+    {
+        [Column(Name = "No", IsPrimaryKey = true)]
+        public int No { get; set; }
+        [Column(Name = "title")]
+        public String Title { get; set; }
+        [Column(Name = "description")]
+        public String Description { get; set; }
+        [Column(Name = "type", IsPrimaryKey = true)]
+        public int Type { get; set; }
+        [Column(Name = "content")]
+        public String Content { get; set; }
+        [Column(Name = "output")]
+        public String Output { get; set; }
+    }
+
+    [Table(Name = "TYPE_TABLE")]
+    class ReferenceTypeTable
+    {
+        [Column(Name = "type", IsPrimaryKey = true)]
+        public int Type { get; set; }
+        [Column(Name = "type_content")]
+        public String TypeContent { get; set; }
+    }
+}
