7372a69097de2ba5eee6f6e2ec36520afb20e1d4
a <a>
2024-05-11 21:09:20 +0900

    接続先の修正

--- PromptSearchTool/PgDbContext.cs
+++ PromptSearchTool/PgDbContext.cs
@@ -12,7 +12,7 @@ namespace PromptSearchTool
 {
     class PgDbContext : DbContext
     {
-        private const string ConnectionString = "Server=localhost;User ID=root;Password=root;Database=postgres;port=5432";
+        private const string ConnectionString = "Server=localhost;User ID=postgres;Password=root;Database=postgres;port=5432";
 
         // コンストラクタにて接続文字列を設定
         public PgDbContext() : base(new NpgsqlConnection(ConnectionString), true) { }
